6b0bf2af06eeb239044905831ec6702c2e035ec0
Kalglover <111890647+Kalglover@users.noreply.github.com>
2024-08-22 16:25:33 -0400

    Add files via upload

---
+++ Aug-22.R
@@ -0,0 +1,8 @@
+#Author:Kalia Glover,Date:08/22/24, Purpose:Test the measures of decriptive stats
+#How to calcuate standard deviation in R
+X <-c (1,2,3,4,5)
+sd(X)
+#How the calculate mean in R 
+mean(X)
+#How to calculate median in R 
+median(X)
