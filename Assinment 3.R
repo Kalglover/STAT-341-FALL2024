0539da58093940c41f602920ea25ba42f3e2c047
Kalglover <111890647+Kalglover@users.noreply.github.com>
2024-08-29 16:20:43 -0400

    Add files via upload

---
+++ Assinment 3.R
@@ -0,0 +1,10 @@
+#Author:Kalia Glover,Date:08/29/24
+# Create 1000 random numbers and store them in a variable called variable 1
+variable1 <- runif(n=1000, min=1, max=1000)
+
+#median(variable1)
+[1] 0.5211244
+#sd(variable1)
+[1] 0.2845426
+#median(variable1)
+[1] 0.5211244
